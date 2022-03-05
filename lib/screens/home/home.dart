cf6da645fd999bcb975e4d95afa1ce8283f771bd
tatarmagda <tatar.magdalena@wp.pl>
2022-03-05 14:23:54 +0100

    log out

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_example/screens/auth/data/providers/auth_state.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,7 +10,12 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text("Home"),
+        child: TextButton(
+          child: Text("log out"),
+          onPressed: () {
+            Provider.of<AuthState>(context, listen: false).signOutWithEmail();
+          },
+        ),
       ),
     );
   }
